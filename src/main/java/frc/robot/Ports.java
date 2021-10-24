a5cd7e78870b6d21766ac567e0fa56b202d07fd6
eitan <eitan.vichik@gmail.com>
2021-10-24 19:24:25 +0300

    Code for Elevator

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class Ports {
+    public static class Elevator{
+        public static int sensorPos = 0;
+        public static int eleMotor = 0;
+        public static int xbox = 0;
+        public static int pidSlot = 0;
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,8 +10,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.ExampleSubsystem.ExampleSubsystem;
-import frc.robot.subsystems.ExampleSubsystem.commands.ExampleCommand;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import frc.robot.subsystems.Elevator.commands.UpAndDownUwu;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -20,12 +20,14 @@ import frc.robot.subsystems.ExampleSubsystem.commands.ExampleCommand;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  public Intake intake = new Intake();
-  public XboxController Xbox = new XboxController(1);
-  public JoystickButton a = new JoystickButton(Xbox, XboxController.Button.kA.value);
-  public JoystickButton b = new JoystickButton(Xbox, XboxController.Button.kB.value);
+  private Button a = new Button();
+  private boolean UpOrDown = true;
+
   // The robot's subsystems and commands are defined here...
 
+  public void setUpOrDown(boolean upOrDown){
+    this.UpOrDown = upOrDown;
+  }
 
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -42,8 +44,7 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    a.whileHeld(new EatCommand(intake, 0.5));
-    b.whenPressed(new Chomp(Intake, Intake.state.OPEN))
+    a.whenPressed(new UpAndDownUwu(UpOrDown));
   }
 
 
