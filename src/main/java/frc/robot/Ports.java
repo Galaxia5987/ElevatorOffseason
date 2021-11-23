0988b82ccf5137141cc4855b4afe4884256dd4c2
yuval <yuvallaif@gmail.com>
2021-11-23 16:25:11 +0200

    Add base code

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,15 +1,7 @@
 package frc.robot;
 
 public class Ports {
-    public static class Elevator{
-        public static final boolean SENSOR_PHASE = true;
-        public static final boolean MOTOR_INVERTED = true;
-        public static final int PID_P = 0;
-        public static final int PID_I = 0;
-        public static final int PID_D = 0;
-        public static final int PID_F = 0;
-        public static final int TOP_LIMIT_SWITCH = 0;
-        public static final int BOTTOM_LIMIT_SWITCH = 1;
-    }
+    public static class Gripper {
 
+    }
 }
