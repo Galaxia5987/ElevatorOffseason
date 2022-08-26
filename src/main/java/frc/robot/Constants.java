9335784de08e17d099779fa86499f018e7e3adfe
Emma03 <emmalivne.03@gmail.com>
2022-08-26 12:48:14 +0300

    add constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,5 +23,6 @@ public final class Constants {
     public static final int MOTION_ACCELERATION=0;
     public static final int CRUISE_VELOCITY=0;
     public static final double DEAD_BEND=0.05;
+    public static final double ELEVATOR_THRESHOLD=0.3;
 
 }
