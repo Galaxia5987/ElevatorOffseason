a56af4260e3df12902b6538ddc34a1633cf7372f
Emma03 <emmalivne.03@gmail.com>
2022-08-26 12:47:05 +0300

    add constructor and override methods

--- src/main/java/frc/robot/commands/PositionControl.java
+++ src/main/java/frc/robot/commands/PositionControl.java
@@ -1,6 +1,35 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ExampleSubsystem.Elevator;
 
 public class PositionControl extends CommandBase {
+    private final Elevator elevator;
+    private final double height;
+
+    public PositionControl(Elevator elevator, double height) {
+        this.elevator = elevator;
+        this.height=height;
+        addRequirements(elevator);
+    }
+
+    @Override
+    public void execute() {
+        elevator.setPosition(height);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevator.setPower(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if(elevator.heightCheck(elevator.getPosition(), height)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
