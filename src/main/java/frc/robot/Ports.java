4da0654c68d8782e0e7facd786c93b38d21e4b06
Emma03 <emmalivne.03@gmail.com>
2022-08-26 11:14:58 +0300

    add motor port

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,15 +1,5 @@
 package frc.robot;
 
 public class Ports {
-    public static class Gripper {
-        public static final int LEFTMOTOR = 0;
-        public static final int RIGHTMOTOR = 0;
-        public static final int SOLENOID = 0;
-
-        public static final boolean RIGHT_MOTOR_INVERTED = false;
-        public static final boolean LEFT_MOTOR_INVERTED = false;
-    }
-    public static class Controller {
-        public static final int XBOX_CONTROLLER = 0;
-    }
+    public static final int MOTOR=0;
 }
