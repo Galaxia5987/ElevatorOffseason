c3f890722f77c8a01e703c283f0db57f16c8d3d5
Emma03 <emmalivne.03@gmail.com>
2022-08-26 11:20:35 +0300

    add invert type port

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+
 public class Ports {
     public static final int MOTOR=0;
+    public static final TalonFXInvertType INVERTED = TalonFXInvertType.Clockwise;
+
 }
