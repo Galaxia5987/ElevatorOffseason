7251e87673f7bd4030dd2c51f6e773a13aa37c8f
Emma03 <emmalivne.03@gmail.com>
2022-08-26 11:50:02 +0300

    add constructor and override methods to command

--- src/main/java/frc/robot/commands/MoveElevator.java
+++ src/main/java/frc/robot/commands/MoveElevator.java
@@ -1,2 +1,32 @@
-package frc.robot.commands;public class MoveElevator {
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ExampleSubsystem.Elevator;
+
+import java.util.function.DoubleSupplier;
+
+public class MoveElevator extends CommandBase {
+        private final Elevator elevator;
+        private final DoubleSupplier doubleSupplier;
+
+        public MoveElevator(Elevator elevator, DoubleSupplier doubleSupplier){
+            this.elevator=elevator;
+            this.doubleSupplier=doubleSupplier;
+            addRequirements(elevator);
+        }
+
+    @Override
+    public void execute() {
+        elevator.setPower();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
