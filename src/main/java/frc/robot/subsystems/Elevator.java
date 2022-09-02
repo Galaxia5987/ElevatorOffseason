7a869ecbf901abded5a557bca18a667ef5b0bfd9
jonathan <jonathan.lotan@gmail.com>
2022-09-02 13:14:41 +0300

    add setPosition & getPosition

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -9,7 +10,7 @@ import frc.robot.Ports;
 public class Elevator extends SubsystemBase {
     private final WPI_TalonFX motor = new WPI_TalonFX(Ports.Elevator.MOTOR);
     private Elevator INSTANCE = null;
-
+    private final UnitModel unitModel = new UnitModel(Constants.Elevator.TICKS_PER_METER);
     private Elevator(){
         motor.setInverted(Ports.Elevator.INVERT);
         motor.enableVoltageCompensation(Constants.Elevator.VOLT);
@@ -49,6 +50,17 @@ public class Elevator extends SubsystemBase {
             value = 0;
         }
         return value;
+    }
+
+    /**
+     * set position to pos
+     * @param pos
+     */
+    public void setPosition(double pos){
+        motor.set(ControlMode.Position, unitModel.toTicks(pos));
+    }
+    public double getPosition(){
+        return unitModel.toUnits(motor.getSelectedSensorPosition());
     }
 
 }
