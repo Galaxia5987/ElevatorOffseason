ea7c5cf870e784eb7626b01194d0dd8b77ce7e90
Jonathan <jonathan.lotan@gmail.com>
2022-09-09 13:01:18 +0300

    Create Height command

--- src/main/java/frc/robot/subsystems/Elevator/Height.java
+++ src/main/java/frc/robot/subsystems/Elevator/Height.java
@@ -1,16 +1,32 @@
 package frc.robot.subsystems.Elevator;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
-public class Height extends SubsystemBase {
+public class Height extends CommandBase {
     private final double height;
     private Elevator elevator;
 
     public Height(double height, Elevator elevator) {
         this.height = height;
         this.elevator = elevator;
-        
+        addRequirements(elevator);
     }
 
 
+    @Override
+    public void execute() {
+        elevator.setPosition(height);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevator.setPower(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Math.abs(elevator.getPosition() - height) <= Constants.Elevator.OFFSET;
+    }
 }
